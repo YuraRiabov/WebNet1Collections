36c8acc33e298f1385a19bfd5946dbb26a051741
YuraRiabov <uraryabov@gmail.com>
2023-09-17 00:41:26 +0300

    Create enqueue, dequeue and peek methods.

--- Collections/MyQueue.cs
+++ Collections/MyQueue.cs
@@ -16,6 +16,68 @@ public class MyQueue<T> : IEnumerable<T>
         }
     }
 
+    public MyQueue()
+    {
+        
+    }
+
+    public MyQueue(IEnumerable<T> source)
+    {
+        foreach (var value in source)
+        {
+            Enqueue(value);
+        }
+    }
+
+    public void Enqueue(T value)
+    {
+        var newNode = new MyQueueNode<T>(value);
+        
+        if (_first is null)
+        {
+            _first = newNode;
+            return;
+        }
+
+        if (_last is null)
+        {
+            _first.Next = newNode;
+            _last = newNode;
+            return;
+        }
+
+        _last.Next = newNode;
+        _last = newNode;
+    }
+
+    public T Dequeue()
+    {
+        if (_first is null)
+        {
+            throw new InvalidOperationException();
+        }
+        
+        var first = _first;
+        _first = _first.Next;
+
+        if (_first is not null && !first.HasNext)
+        {
+            _last = null;
+        }
+        
+        return first.Value;
+    }
+
+    public T Peek()
+    {
+        if (_first is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return _first.Value;
+    }
+
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
