2de33a3853ac95e6f52a214efef6a4f883bb7541
YuraRiabov <uraryabov@gmail.com>
2023-10-02 15:45:36 +0300

    Add dequeue and peek tests.

--- Collections.Test.Unit/MyQueueTests.cs
+++ Collections.Test.Unit/MyQueueTests.cs
@@ -31,28 +31,143 @@ public class MyQueueTests
             queue.Enqueue(value);
         }
 
-        using var enumerator = queue.GetEnumerator();
-        foreach (T value in values)
-        {
-            enumerator.MoveNext();
-            var currentValue = enumerator.Current;
-            
-            Assert.Equal(value, currentValue);
-        }
+        AssertEqualCollections(values, queue);
     }
-    
+
     [Theory]
     [MemberData(nameof(GetTestDataForQueueFill))]
     public void EnumerableConstructor_ShouldAddElements<T>(T[] values)
     {
         var queue = new MyQueue<T>(values);
 
+        AssertEqualCollections(values, queue);
+    }
+
+
+    [Theory]
+    [MemberData(nameof(GetTestDataForQueueFill))]
+    public void Dequeue_WhenNotEmptyQueue_ShouldReturnAndRemove<T>(T[] values)
+    {
+        var queue = new MyQueue<T>(values);
+        
+        foreach (T value in values)
+        {
+            var currentValue = queue.Dequeue();
+
+            Assert.Equal(value, currentValue);
+        }
+        
+        Assert.Empty(queue);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetEmptyQueuesTestData))]
+    public void Dequeue_WhenEmptyQueue_ShouldThrow<T>(MyQueue<T> queue)
+    {
+        var call = () =>
+        {
+            queue.Dequeue();
+        };
+
+        Assert.Throws<InvalidOperationException>(call);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetTestDataForQueueFill))]
+    public void TryDequeue_WhenNotEmptyQueue_ShouldReturnTrueAndRemove<T>(T[] values)
+    {
+        var queue = new MyQueue<T>(values);
+
+        foreach (T value in values)
+        {
+            bool tryDequeueResult = queue.TryDequeue(out var currentValue);
+
+            Assert.Equal(value, currentValue);
+            Assert.True(tryDequeueResult);
+        }
+        
+        Assert.Empty(queue);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetEmptyQueuesTestData))]
+    public void TryDequeue_WhenEmptyQueue_ShouldReturnFalse<T>(MyQueue<T> queue)
+    {
+        var tryDequeueResult = queue.TryDequeue(out var currentValue);
+        
+        Assert.Empty(queue);
+        Assert.False(tryDequeueResult);
+        Assert.Equal(default, currentValue);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetTestDataForQueueFill))]
+    public void Peek_WhenNotEmptyQueue_ShouldReturnAndNotRemove<T>(T[] values)
+    {
+        var queue = new MyQueue<T>(values);
+        
+        foreach (T value in values)
+        {
+            var currentValue = queue.Peek();
+
+            Assert.Equal(value, currentValue);
+
+            queue.Dequeue();
+        }
+        
+        Assert.Empty(queue);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetEmptyQueuesTestData))]
+    public void Peek_WhenEmptyQueue_ShouldThrow<T>(MyQueue<T> queue)
+    {
+        var call = () =>
+        {
+            queue.Peek();
+        };
+
+        Assert.Throws<InvalidOperationException>(call);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetTestDataForQueueFill))]
+    public void TryPeek_WhenNotEmptyQueue_ShouldReturnTrueAndNotRemove<T>(T[] values)
+    {
+        var queue = new MyQueue<T>(values);
+
+        foreach (T value in values)
+        {
+            bool tryDequeueResult = queue.TryPeek(out var currentValue);
+
+            Assert.Equal(value, currentValue);
+            Assert.True(tryDequeueResult);
+
+            queue.Dequeue();
+        }
+        
+        Assert.Empty(queue);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetEmptyQueuesTestData))]
+    public void TryPeek_WhenEmptyQueue_ShouldReturnFalse<T>(MyQueue<T> queue)
+    {
+        var tryDequeueResult = queue.TryPeek(out var currentValue);
+        
+        Assert.Empty(queue);
+        Assert.False(tryDequeueResult);
+        Assert.Equal(default, currentValue);
+    }
+
+    private static void AssertEqualCollections<T>(IEnumerable<T> values, IEnumerable<T> queue)
+    {
         using var enumerator = queue.GetEnumerator();
         foreach (T value in values)
         {
             enumerator.MoveNext();
             var currentValue = enumerator.Current;
-            
+
             Assert.Equal(value, currentValue);
         }
     }
